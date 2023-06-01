7730c910c864ec7402da85d4b12c5c9c54d294e2
ngohongthong <baphongpine@gmail.com>
2023-06-01 23:09:29 +0700

    complete idea for UI homeSearch

--- my-app/src/components/homeDefault/index.jsx
+++ my-app/src/components/homeDefault/index.jsx
@@ -1,6 +1,26 @@
+import classNames from "classnames/bind"
+
+import styles from "./homeDefault.module.scss"
+
+const cx = classNames.bind(styles)
+
+
 const HomeDefault = () => {
     return (
-        <h1>Home default</h1>
+        <div className={cx("homeDefault__wrap")}>
+            <div className={cx("homeDefault__wrap__content")}>
+                <div className={cx("homeDefault__wrap__content__title")}>
+                    <h1 className={cx("homeDefault__wrap__content__title__text")}>Welcome to my website</h1>
+                </div>
+            </div>
+            <div className={cx("homeDefault__wrap__footer")}>
+                <div className={cx("homeDefault__wrap__footer__content")}>
+                    <div className={cx("homeDefault__wrap__footer__content__title")}>
+                        <h1 className={cx("homeDefault__wrap__footer__content__title__text")}>Footer</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
--- my-app/src/components/homeSearch/index.jsx
+++ my-app/src/components/homeSearch/index.jsx
@@ -1,6 +1,61 @@
+import classnames from 'classnames/bind'
+import { useEffect, useState } from 'react'
+
+import styles from './homeSearch.module.scss'
+
+const cx = classnames.bind(styles)
+
+
 const HomeSearch = () => {
+
+    const [show, setShow] = useState(0)
+
+    useEffect(() => {
+        if (show <= 3) {
+            setTimeout(() => {
+                setShow(show + 1)
+            }, 1000)
+        }else {
+            setShow(0)
+        }
+    }, [show])
+
     return (
-        <h1>Home search</h1>
+        <div className={cx("homeSearch", "container lg:w-5/6 xl:w-4/5 2xl:w-4/6 mx-2")}>
+            <div className={cx("homeSearch__header", "p-1.5")}>
+                <div className={cx("homeSearch__header__title")}>Kết quả tìm kiếm cho : transformer 6 </div>
+            </div>
+            <div className={cx("homeSearch__filter__wrap", "p-1.5")}>
+                <div className={cx("homeSearch__filter__wrap__item", "grid grid-cols-4 gap-4")}>
+                    <div className={cx(`homeSearch__filter__wrap__item__title${show === 0 ? "--active" : ""}`)}>
+                        <p className={cx("homeSearch__filter__wrap__item__title__text")}>Search with name</p>
+                    </div>
+                    <div className={cx(`homeSearch__filter__wrap__item__title${show === 1 ? "--active" : ""}`)}>
+                        <p className={cx("homeSearch__filter__wrap__item__title__text")}>Search with genre</p>
+                    </div>
+                    <div className={cx(`homeSearch__filter__wrap__item__title${show === 2 ? "--active" : ""}`)}>
+                        <p className={cx("homeSearch__filter__wrap__item__title__text")}>Search with cast</p>
+                    </div>
+                    <div className={cx(`homeSearch__filter__wrap__item__title${show === 3 ? "--active" : ""}`)}>
+                        <p className={cx("homeSearch__filter__wrap__item__title__text")}>Search with director</p>
+                    </div>
+                </div>
+            </div>
+            <div className={cx("homeSearch__content", "p-1.5")}>
+                <div className={cx("homeSearch__content__wrap__item","grid grid-cols-5 gap-5")}>
+                    <div className={cx("item")}></div>
+                    <div className={cx("item")}></div>
+                    <div className={cx("item")}></div>
+                    <div className={cx("item")}></div>
+                    <div className={cx("item")}></div>
+                    <div className={cx("item")}></div>
+                    <div className={cx("item")}></div>
+                    <div className={cx("item")}></div>
+                    <div className={cx("item")}></div>
+                    <div className={cx("item")}></div>
+                </div>
+            </div>
+        </div>
     )
 }
 
