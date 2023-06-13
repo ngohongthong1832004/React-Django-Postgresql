afe94f9374c85a93fad141c37f014908733c1bb9
ngohongthong <baphongpine@gmail.com>
2023-06-13 23:10:46 +0700

    add axios and test fetch data from django server

--- my-app/src/components/homeDefault/index.jsx
+++ my-app/src/components/homeDefault/index.jsx
@@ -1,10 +1,112 @@
 import classNames from "classnames/bind";
+import {useEffect, useState} from "react";
+import axios from 'axios';
+
 
 import styles from "./homeDefault.module.scss";
 
 const cx = classNames.bind(styles);
 
 const HomeDefault = () => {
+
+  // useEffect(() => {
+  //   fetch('http://127.0.0.1:8000/api/all')
+  //     .then(response => {
+  //       console.log("da vao day")
+  //       if (!response.ok) {
+  //         throw new Error('Network response was not OK');
+  //       }
+  //       return response.json();
+  //     })
+  //     .then(json => {
+  //       console.log(json);
+  //     })
+  //     .catch(error => {
+  //       console.log(error);
+  //     });
+  // }, []);
+
+  const [token, setToken] = useState('')
+  // useEffect(() => {
+  //   axios.post('http://127.0.0.1:8000/login/', {
+  //     username: 'admin',
+  //     password: '123',
+  //   })
+  // .then(response => {
+  //   // Handle the response
+  //   setToken(response.data.token)
+  //   console.log(response.data);
+  // })
+  // .catch(error => {
+  //   // Handle the error
+  //   console.error(error);
+  // });
+  // }, []);
+
+  // useEffect(() => {
+  //   axios.post('http://127.0.0.1:8000/logout/', null, {
+  //     headers: {
+  //       'Content-Type': 'application/json',
+  //       Authorization: `Token ${token}`,
+  //     }
+  //   })
+  //   .then(response => {
+  //     // Handle successful logout
+  //     console.log(response.data);
+  //   })
+  //   .catch(error => {
+  //     // Handle error
+  //     console.error('Logout failed:', error);
+  //   });
+  // }, []);
+
+
+  useEffect(() => {
+    axios
+      .post('http://127.0.0.1:8000/login/', {
+        username: 'admin',
+        password: '123',
+      })
+      .then(response => {
+        // Handle the response
+        setToken(response.data.token);
+        console.log(response.data);
+  
+        axios
+          .post('http://127.0.0.1:8000/logout/', null, {
+            headers: {
+              'Content-Type': 'application/json',
+              Authorization: `Token 022e64b05f5b3939379b95a8c4a44463e109eb65`,
+            },
+          })
+          .then(response => {
+            // Handle successful logout
+            console.log(response.data);
+          })
+          .catch(error => {
+            // Handle error
+            console.error('Logout failed:', error);
+          });
+      })
+      .catch(error => {
+        // Handle the error
+        console.error(error);
+      });
+  }, []);
+  
+
+
+
+
+  // useEffect(() => {
+  //   fetch('https://jsonplaceholder.typicode.com/todos/1')
+  //     .then(response => response.json())
+  //     .then(json => console.log(json))
+  // }, []);
+
+
+
+
   return (
     <div
       className={cx(
